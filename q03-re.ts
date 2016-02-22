6ce4b594daec04f23f1aa20bd69d62b1c6c1691c
arukmn <t.kuriyama@air-closet.com>
2016-02-23 03:14:17 +0900

    create q03 by typescript

---
+++ q03-re.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+class InsideOutGame {
+  private MAX: number;
+  private cards: boolean[];
+
+  constructor() {
+    this.MAX = 100;
+    this.cards = [];
+
+    for (var i = 0; i < this.MAX; i++) {
+      this.cards.push(false);
+    }
+  }
+
+  private logging(): void {
+    for (var i = 0; i < this.MAX; i++) {
+      if (this.cards[i] === false) {
+        console.log('cards:', i + 1);
+      }
+    }
+  }
+
+  private exec(interval: number): void {
+    var start = interval - 1;
+    for (var i = start; i < this.MAX; i += interval) {
+      this.cards[i] = !this.cards[i];
+    }
+  }
+
+  public start(val: number): void {
+    for (var i = val; i <= this.MAX; i++) {
+      this.exec(i);
+    }
+
+    this.logging();
+  }
+}
+
+new InsideOutGame().start(2);
