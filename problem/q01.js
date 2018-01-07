395fa3c106b10fcd6484a7a5748eacd272a84aad
koolii <koolii0909@gmail.com>
2018-01-07 05:30:44 +0100

    :+1: add problem01's memo

--- problem/q01.js
+++ problem/q01.js
@@ -1,17 +1,21 @@
 const reverse = str => str.split('').reverse().join('')
 
+  // 左が必ず1にならないと行けない
+  // よって右端も1でなければならない = 奇数である
 module.exports = () => {
+  // 10以上の奇数
   const initial = 11
 
-  for (let i = initial; ; i += 2) {
-    const str10 = i.toString()
-    const str8 = i.toString(8)
-    const str2 = i.toString(2)
+  for (let num = initial; ; num += 2) {
+    const str10 = num.toString()
+    const str8 = num.toString(8)
+    const str2 = num.toString(2)
 
+    // 全ての基数の文字列が逆順と同一である
     if (str10 === reverse(str10)
     && str8 === reverse(str8)
     && str2 === reverse(str2)) {
-      return i
+      return num
     }
   }
 }
