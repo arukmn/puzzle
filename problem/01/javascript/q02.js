adca7b07f1eecd67a1624586a517642a859c4e85
koolii <koolii0909@gmail.com>
2018-01-09 21:58:45 +0100

    :+1: add memo

--- problem/01/javascript/q02.js
+++ problem/01/javascript/q02.js
@@ -2,6 +2,14 @@
 const ope = ['+', '-', '*', '/', '']
 const opeLen = ope.length
 
+/*
+* ここで大事なのはope変数に空文字列を追加してることで、符号を付けないパターンを実現している所
+* 気をつけなければならないのはequationを作成している箇所
+* 実際に失敗したが、一番深いfor文内で作成しないと正しい値にならない
+* 途中のfor文でequationを順々に付加させてしまうと、一番深いfor文で何度も付加されてしまい、四項の計算ではなくなる
+*
+*/
+
 module.exports = (init = 1000, end = 10000) => {
   for (let i = init; i < end; i += 1) {
     const nums = String(i).split('')
